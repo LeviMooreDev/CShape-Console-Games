5b88b6739b57979b7ac447c96c41fbf13a8552c7
LeviMoore <mail@levimoore.dk>
2021-02-02 01:01:32 +0100

    added static left, right, up, down

--- Engine/Vector2I.cs
+++ Engine/Vector2I.cs
@@ -1,9 +1,15 @@
-﻿namespace Engine
+namespace Engine
 {
     public struct Vector2I
     {
         public int x;
         public int y;
+
+        public static Vector2I Zero => new Vector2I(0, 0);
+        public static Vector2I Left => new Vector2I(-1, 0);
+        public static Vector2I Right => new Vector2I(1, 0);
+        public static Vector2I Up => new Vector2I(0, 1);
+        public static Vector2I Down => new Vector2I(0, -1);
 
         public Vector2I(int x, int y)
         {
