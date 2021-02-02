ab4336e75d8797759824315264f156e0b6e7cd92
LeviMoore <mail@levimoore.dk>
2021-02-02 17:43:30 +0100

    added Rng class

---
+++ Engine/Rng.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public static class Rng
+    {
+        private static Random random = new Random();
+
+        public static float Value => (float)random.NextDouble();
+
+        public static int Range(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+        public static double Range(double minimum, double maximum)
+        {
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+        public static float Range(float minimum, float maximum)
+        {
+            return (float)Range(minimum, maximum);
+        }
+    }
+}
